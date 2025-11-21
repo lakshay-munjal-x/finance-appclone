4151a30eed18e3e7b34202cfcf66f8355b834e1d
lakshay-Munjal-x <lakshayatwork06@gmail.com>
2025-11-21 13:52:20 +0530

    3rd

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log('heyyy');
+console.error("For wrong statement");
+console.warn("for warning statement ");
+console.log(1+12345678);
+ var name = "Lakshay";
+// var name = "Lakshay"
+// let name = "Lakshay"
+// const name = "Lakshay"
+var name; //value is undefined
+var divya = "Lakshay"; // initialisation
+divya = "formation"; //we can assign value or reassign value through var 
+// let name="arpit" //it gives error because it doesnt do redeclaration 
+const ketan = "some";
+//ketan = "house" //we cant even change value
+console.log(ketan)
+console.log(divya)
+console.log(typeof "hiloo")
+console.log(typeof true)
+console.log(typeof undefined) //if we cant give value so it is defined automatically and thevalue remains "undefined"
+console.log(typeof null) // if we miss current value and will asign later then it should be null
